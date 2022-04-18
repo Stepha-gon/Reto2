98677d67d1d61204866837633b9a3857e4d4987b
Stephanie Gonzalez <79724950+Stepha-gon@users.noreply.github.com>
2022-04-17 19:55:38 -0500

    Add files via upload

---
+++ Capaentidad/Classentidad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capaentidad
+{
+    public class Classentidad
+    {
+        public String nombre { get; set; }
+        public String code { get; set; }
+        public String autor { get; set; }
+        public String editor { get; set; }
+        public double price { get; set; }
+        public int cant { get; set; }
+        public String accion { get; set; }
+
+
+
+    }
+}
