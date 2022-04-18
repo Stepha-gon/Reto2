9843d7f07cc4d5bdcc745c248467863484654083
Stephanie Gonzalez <79724950+Stepha-gon@users.noreply.github.com>
2022-04-17 19:56:33 -0500

    Add files via upload

---
+++ reto2/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace reto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cant = new System.Windows.Forms.TextBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.editor = new System.Windows.Forms.TextBox();
+            this.autor = new System.Windows.Forms.TextBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.Code = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buscarv = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.insertarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cant);
+            this.groupBox1.Controls.Add(this.price);
+            this.groupBox1.Controls.Add(this.editor);
+            this.groupBox1.Controls.Add(this.autor);
+            this.groupBox1.Controls.Add(this.nombre);
+            this.groupBox1.Controls.Add(this.Code);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(782, 196);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Libros";
+            // 
+            // cant
+            // 
+            this.cant.Location = new System.Drawing.Point(441, 143);
+            this.cant.Name = "cant";
+            this.cant.Size = new System.Drawing.Size(176, 22);
+            this.cant.TabIndex = 11;
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(441, 85);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(176, 22);
+            this.price.TabIndex = 10;
+            this.price.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // editor
+            // 
+            this.editor.Location = new System.Drawing.Point(441, 30);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(176, 22);
+            this.editor.TabIndex = 9;
+            // 
+            // autor
+            // 
+            this.autor.Location = new System.Drawing.Point(98, 146);
+            this.autor.Name = "autor";
+            this.autor.Size = new System.Drawing.Size(186, 22);
+            this.autor.TabIndex = 8;
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(98, 85);
+            this.nombre.Name = "nombre";
+            this.nombre.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.nombre.Size = new System.Drawing.Size(186, 22);
+            this.nombre.TabIndex = 7;
+            // 
+            // Code
+            // 
+            this.Code.Enabled = false;
+            this.Code.Location = new System.Drawing.Point(98, 31);
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(186, 22);
+            this.Code.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(364, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cantidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(364, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Editorial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Autor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Titulo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buscarv);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(13, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(769, 102);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscar";
+            // 
+            // buscarv
+            // 
+            this.buscarv.Location = new System.Drawing.Point(137, 40);
+            this.buscarv.Name = "buscarv";
+            this.buscarv.Size = new System.Drawing.Size(330, 22);
+            this.buscarv.TabIndex = 2;
+            this.buscarv.TextChanged += new System.EventHandler(this.buscarv_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 17);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Buscar codigo";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(13, 393);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(782, 358);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tabla de datos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 302);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertarToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.eliminarToolStripMenuItem1,
+            this.nuevoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // insertarToolStripMenuItem
+            // 
+            this.insertarToolStripMenuItem.Name = "insertarToolStripMenuItem";
+            this.insertarToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.insertarToolStripMenuItem.Text = "Insertar";
+            this.insertarToolStripMenuItem.Click += new System.EventHandler(this.insertarToolStripMenuItem_Click);
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.editarToolStripMenuItem.Text = "Editar";
+            this.editarToolStripMenuItem.Click += new System.EventHandler(this.editarToolStripMenuItem_Click);
+            // 
+            // eliminarToolStripMenuItem1
+            // 
+            this.eliminarToolStripMenuItem1.Name = "eliminarToolStripMenuItem1";
+            this.eliminarToolStripMenuItem1.Size = new System.Drawing.Size(77, 24);
+            this.eliminarToolStripMenuItem1.Text = "Eliminar";
+            this.eliminarToolStripMenuItem1.Click += new System.EventHandler(this.eliminarToolStripMenuItem1_Click);
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.nuevoToolStripMenuItem.Text = "Limpiar";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 792);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox editor;
+        private System.Windows.Forms.TextBox autor;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.TextBox Code;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cant;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox buscarv;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem1;
+    }
+}
+
---
+++ reto2/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Capaentidad;
+using Capanegocio;
+namespace reto2
+{
+    public partial class Form1 : Form
+    {
+        Classentidad objent = new Classentidad();
+        Classnegocio objneg = new Classnegocio();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void insertar (String accion)
+        {
+            objent.code = Code.Text;
+            objent.nombre = nombre.Text;
+            objent.autor = autor.Text;
+            objent.editor = editor.Text;
+            objent.price = Convert.ToDouble(price.Text);
+            objent.cant = Convert.ToInt32(cant.Text);
+            objent.accion = accion;
+            String message = objneg.n_insertar_libros(objent);
+            MessageBox.Show(message, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        void limpiar()
+        {
+            Code.Text="";
+            nombre.Text = "";
+            autor.Text = "";
+            editor.Text = "";
+            price.Text = "";
+            cant.Text = "";
+            buscarv.Text = "";
+            dataGridView1.DataSource = objneg.n_listar_libros();
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = objneg.n_listar_libros();
+
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+        private void insertarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Code.Text == "")
+            {
+                if (MessageBox.Show("¿Registrar Dato?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    insertar("1");
+                    limpiar();
+                }
+            }
+                
+
+        }
+        private void editarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Code.Text != "")
+            {
+                if (MessageBox.Show("¿editar Dato?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    insertar("2");
+                    limpiar();
+                }
+            }
+        }
+        private void eliminarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (Code.Text != "")
+            {
+                if (MessageBox.Show("¿Eliminar Dato?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    insertar("3");
+                    limpiar();
+                }
+            }
+
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buscarv_TextChanged(object sender, EventArgs e)
+        {
+
+            if (buscarv.Text!="")
+            {
+                objent.nombre = nombre.Text;
+                DataTable dt = new DataTable();
+                dt = objneg.n_buscar_libros(objent);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                dataGridView1.DataSource = objneg.n_listar_libros();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int fila = dataGridView1.CurrentCell.RowIndex;
+            Code.Text = dataGridView1[0,fila].Value.ToString();
+            nombre.Text = dataGridView1[1, fila].Value.ToString();
+            autor.Text = dataGridView1[2, fila].Value.ToString();
+            editor.Text = dataGridView1[3, fila].Value.ToString();
+            price.Text = dataGridView1[4, fila].Value.ToString();
+            cant.Text = dataGridView1[5, fila].Value.ToString();
+        }
+    }
+}
