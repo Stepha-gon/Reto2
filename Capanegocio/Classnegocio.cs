dadce3f61434cf7df14e59e8e7e45ee89e6b88f5
Stephanie Gonzalez <79724950+Stepha-gon@users.noreply.github.com>
2022-04-17 19:56:00 -0500

    Add files via upload

---
+++ Capanegocio/Classnegocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using Capadatos;
+using Capaentidad;
+namespace Capanegocio
+{
+    public class Classnegocio
+    {
+        Classdatos objn = new Classdatos();
+
+        public DataTable n_listar_libros()
+        {
+            return objn.d_listar_libros();
+        }
+
+        public DataTable n_buscar_libros(Classentidad obje)
+        {
+            return objn.d_buscar_libros(obje);
+        }
+        public String n_insertar_libros(Classentidad obje)
+        {
+            return objn.d_insertar_libros(obje);
+        }
+
+    }
+}
